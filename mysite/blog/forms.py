3913441537490601f3576e1323706d3c58350a59
自宅PC <sai11121209@gmail.com>
2020-06-18 19:47:07 +0900

    #15 Postテンプレート修正

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -4,9 +4,9 @@ from .models import Posts
 class PostForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['title'] = forms.CharField(label='タイトル', widget=forms.TextInput(attrs={'placeholder':'タイトル'}))
-        self.fields['text'] = forms.CharField(label='本文', widget=forms.Textarea(attrs={'placeholder':'記入欄'}))
-        self.fields['tag'] = forms.CharField(label='タグ', widget=forms.TextInput(attrs={'placeholder':'タグをスペース区切りで入力'}))
+        self.fields['title'] = forms.CharField(label='', widget=forms.TextInput(attrs={'class':'title', 'placeholder':'タイトル'}))
+        self.fields['tag'] = forms.CharField(label='', widget=forms.TextInput(attrs={'class':'tag', 'placeholder':'タグをスペース区切りで入力'}))
+        self.fields['text'] = forms.CharField(label='', widget=forms.Textarea(attrs={'class':'text', 'placeholder':'記入欄'}))
 
     class Meta:
         model = Posts
