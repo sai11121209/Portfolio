7875654236daa888b689248cb1c8693cea80d6dd
自宅PC <sai11121209@gmail.com>
2020-06-19 23:52:27 +0900

    #20 accountテストデータ作成

--- mysite/accounts/tests.py
+++ mysite/accounts/tests.py
@@ -1,3 +1,46 @@
 from django.test import TestCase
-
+from django.contrib.auth.models import User
+from faker import Faker
+from django.contrib.auth.hashers import make_password
 # Create your tests here.
+class UserModelTest(TestCase):
+    def test_is_empty(self):
+        saved_posts = User.objects.all()
+        self.assertEqual(saved_posts.count(), 0)
+
+    def test_is_dummydata_ja(self):
+        fake = Faker('ja_JP')
+        dummy_deta = fake.profile()
+        User.objects.create(
+            username=dummy_deta['username'],
+            first_name=dummy_deta['name'].split()[1],
+            last_name=dummy_deta['name'].split()[0],
+            email=dummy_deta['mail'],
+            is_superuser=0,
+            is_staff=0,
+            is_active=1,
+            date_joined=fake.date_time_ad(),
+            last_login=fake.date_time_ad(),
+            password=make_password(fake.password(length=10)),
+            id=1,
+        )
+        saved_user = User.objects.all()
+        self.assertEqual(saved_user.count(), 1)
+    def test_is_dummydata_en(self):
+        fake = Faker()
+        dummy_deta = fake.profile()
+        User.objects.create(
+            username=dummy_deta['username'],
+            first_name=dummy_deta['name'].split()[1],
+            last_name=dummy_deta['name'].split()[0],
+            email=dummy_deta['mail'],
+            is_superuser=0,
+            is_staff=0,
+            is_active=1,
+            date_joined=fake.date_time_ad(),
+            last_login=fake.date_time_ad(),
+            password=make_password(fake.password(length=10)),
+            id=1,
+        )
+        saved_user = User.objects.all()
+        self.assertEqual(saved_user.count(), 1)
--- mysite/blog/tests.py
+++ mysite/blog/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from .models import Posts
+from faker import Faker
 # Create your tests here.
 
 class PostsModelTest(TestCase):
